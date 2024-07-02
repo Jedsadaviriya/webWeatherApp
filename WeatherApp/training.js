a66029298c6bda7c00d8075a7dad2a33d674e11b
Jedsadaviriya <setthawut.j2007@gmail.com>
2024-07-02 19:33:48 +0200

    session 2
    finished session 2 and saving it

--- WeatherApp/training.js
+++ WeatherApp/training.js
@@ -60,7 +60,7 @@ document.getElementById("p2").textContent = `You are ${myage}`;
 document.getElementById("p3").textContent = `Enrolled: ${student}`;
 // { Lesson 2 }
 
-// { Lesson 3 }
+// { Lesson 3 } arithmatic operation
 //arithmetic operators = opprands (values, variables, etc.)
 //                  opertators (+ - * /)
 let students = 30;
@@ -105,3 +105,63 @@ console.log(result);
 // end of session 1 of the video class is starting soon
 // video left of at 32:54
 // { Lesson 3 }
+
+// { Lesson 4 } accept user input
+
+/* this is windows prompt the easy way for user input
+let username;
+// a popup prompt
+username = window.prompt("what is your username?");
+
+console.log(username);
+*/
+
+// this is professional way with the html text box
+let username;
+document.getElementById("mySubmit").onclick = function () {
+  username = document.getElementById("myText").value;
+  document.getElementById("myuser").textContent = `Hey ${username}`;
+};
+// { Lesson 4 }
+
+// { Lesson 5 } type conversion
+let agetc = window.prompt("how old are you?");
+// if the conversion is not here it would be a string so instead of 18 it is 171
+agetc = Number(agetc);
+agetc += 1;
+console.log(agetc, typeof agetc);
+
+let x5 = "0";
+let y5 = "0";
+let z5 = "0";
+
+x5 = Number(x5);
+y5 = String(y5);
+z5 = Boolean(z5);
+
+console.log(x5, typeof x5);
+console.log(y5, typeof y5);
+console.log(z5, typeof z5);
+// { Lesson 5 }
+
+// { Lesson 6 } const
+//let pi = 3.14159; // can be changed
+const PI = 3.14159; // cannot be changed
+let radius;
+let circumference;
+
+radius = window.prompt("enter the radius of a circle");
+radius = Number(radius);
+
+circumference = 2 * PI * radius;
+
+// console.log(circumference);
+
+document.getElementById("mysubmitR").onclick = function () {
+  radius = document.getElementById("mytextR").value;
+  radius = Number(radius);
+  circumference = 2 * PI * radius;
+  document.getElementById("myh3").textContent = circumference + "cm";
+};
+// { Lesson 6 }
+// session 2 left off at 52:30
